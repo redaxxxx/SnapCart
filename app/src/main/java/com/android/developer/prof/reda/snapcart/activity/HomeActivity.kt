4007bb8172f9e61090652d6e1ff8496254a948cc
Reda Nabil <redanabil518@gmail.com>
2024-07-29 13:47:03 +0300

    Dashboard screen

--- app/src/main/java/com/android/developer/prof/reda/snapcart/activity/HomeActivity.kt
+++ app/src/main/java/com/android/developer/prof/reda/snapcart/activity/HomeActivity.kt
@@ -2,12 +2,27 @@ package com.android.developer.prof.reda.snapcart.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.android.developer.prof.reda.snapcart.R
+import com.android.developer.prof.reda.snapcart.databinding.ActivityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        val navController by lazy {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+            navHostFragment.navController
+        }
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
