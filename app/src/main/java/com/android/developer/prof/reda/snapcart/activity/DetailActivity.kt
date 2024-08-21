b2f3a0f04a6eacbdc1f069b90bfcf03ed87c484a
Reda Nabil <redanabil518@gmail.com>
2024-08-21 06:05:32 +0300

    Detail screen

---
+++ app/src/main/java/com/android/developer/prof/reda/snapcart/activity/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.android.developer.prof.reda.snapcart.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.developer.prof.reda.snapcart.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
